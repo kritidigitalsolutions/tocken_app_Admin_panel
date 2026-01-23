1c989fe39c3ebedc4b94a8873ef0bbb7dbdfe846
kritidigitalsolutions <kritidigitalsolutions@gmail.com>
2026-01-23 16:00:39 +0530

    some env adds

--- frontend/src/pages/admin/wallpapers/wallpaperAPI.js
+++ frontend/src/pages/admin/wallpapers/wallpaperAPI.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_API_URL;
+// const API_BASE = process.env.REACT_APP_API_URL || "https://backend-tocken-admin-panel.vercel.app/api";
+// const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 // Get admin token from localStorage
 const getAuthHeader = () => {
